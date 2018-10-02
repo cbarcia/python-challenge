ce2cbed20f66cbb0030614b5c80ced257c7b84a2
cbarcia <42686821+cbarcia@users.noreply.github.com>
2018-10-01 21:03:39 -0400

    commit

--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,17 @@
 #pybank main
+import os
+import csv
+
+# Path to collect data from the folder
+bankcsv = os.path.join('budget_data')
+
+# Read in the CSV file
+with open(wrestlingCSV, 'r') as csvfile:
+
+ # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    # Loop through the data
+    for row in csvreader:
